eabfc0e15d58a07a00aace52ff770ee47e4d4ab7
Subesh Yadav <41501661+subeshyadav3@users.noreply.github.com>
2024-12-17 08:13:25 +0545

    .

--- src/components/pages/home.jsx
+++ src/components/pages/home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import img from './image.png'
 const Home = () => {
 
     
@@ -34,7 +34,7 @@ const Home = () => {
 
                 </div>
 
-                <div class="custom-shape-divider-bottom-1734237866">
+                <div className="custom-shape-divider-bottom-1734237866">
                     <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
                         <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
                     </svg>
@@ -42,13 +42,13 @@ const Home = () => {
             </div>
 
             <div className='h-[400px] flex flex-col sm:flex-row sm:gap-10 md:gap-20 sm:left-5  items-center justify-center gap-2 bg-[#FFF5F5] w-full'>
-                <div className='flex flex-col gap-5 justify-center items-center'>
+                <div className='flex flex-col gap-5 justify-center items-center sm:w-1/2'>
                     <img src="https://www.svgrepo.com/show/33758/idea.svg" className='w-20 h-20' alt="" />
-                    <h1 className='font-poppins text-xl  text-[#942192] font-bold sm:text-2xl sm:ml-5 '>
+                    <h1 className='font-poppins text-xl  text-[#942192] font-bold sm:text-2xl sm:ml-12 '>
                         Pulchowk Campus</h1>
                 </div>
-                <div>
-                    <ul class="list-disc pl-5 mt-3 text-justify px-3 sm:text-xl">
+                <div className='sm:w-1/2'>
+                    <ul class="list-disc pb-10 mt-3 text-justify px-5 sm:text-xl">
                         <li><strong>Reputation:</strong> One of the top engineering campuses in Nepal.</li>
                         <li><strong>Experienced Faculty:</strong> Qualified professors with industry knowledge.</li>
                         <li><strong>Modern Facilities:</strong> Advanced labs and infrastructure for practical learning.</li>
@@ -59,33 +59,34 @@ const Home = () => {
                 </div>
             </div>
 
-            <div className='flex flex-row w-full items-center bg-orange-100'>
-                <div className='w-1/3 flex justify-center items-center  h-[230px]'>
+            <div className='flex flex-col sm:flex-row w-full items-center bg-orange-100'>
+                <div className='sm:w-1/2 flex flex-col gap-3 justify-center items-center pl-5  pt-5 '>
+                    <img src={img} alt=""  className='h-[100px] w-[100px]'/>
                     <button className='text-3xl font-poppins font-bold text-[#942193]'>Faculties</button>
                 </div>
-                <div className='p-10'>
-                    <ul class="relative pl-8 space-y-6 text-xl font-poppins font-bold">
+                <div className='px-5 pb-10 sm:w-1/2'>
+                    <ul className="relative pl-8 space-y-6 text-xl font-poppins font-bold">
 
-                        <div class="absolute left-4 top-0 bottom-0 w-[2px] bg-[#942192]"></div>
+                        <div className="absolute left-4 top-0 bottom-0 w-[2px] bg-[#942192]"></div>
 
-                        <li class="relative flex items-center">
-                            <div class="w-4 h-4 bg-[#942192] rounded-full absolute -left-6"></div>
+                        <li className="relative flex items-center">
+                            <div className="w-4 h-4 bg-[#942192] rounded-full absolute -left-6"></div>
                             Computer Engineering
                         </li>
-                        <li class="relative flex items-center">
-                            <div class="w-4 h-4 bg-[#942192] rounded-full absolute -left-6"></div>
+                        <li className="relative flex items-center">
+                            <div className="w-4 h-4 bg-[#942192] rounded-full absolute -left-6"></div>
                             Civil Engineering2
                         </li>
-                        <li class="relative flex items-center">
-                            <div class="w-4 h-4 bg-[#942192] rounded-full absolute -left-6"></div>
+                        <li className="relative flex items-center">
+                            <div className="w-4 h-4 bg-[#942192] rounded-full absolute -left-6"></div>
                             Eletronics Engineering
                         </li>
-                        <li class="relative flex items-center">
-                            <div class="w-4 h-4 bg-[#942192] rounded-full absolute -left-6"></div>
+                        <li className="relative flex items-center">
+                            <div className="w-4 h-4 bg-[#942192] rounded-full absolute -left-6"></div>
                             Mechanical Engineering
                         </li>
-                        <li class="relative flex items-center">
-                            <div class="w-4 h-4 bg-[#942192] rounded-full absolute -left-6"></div>
+                        <li className="relative flex items-center">
+                            <div className="w-4 h-4 bg-[#942192] rounded-full absolute -left-6"></div>
                             Architecture
                         </li>
                     </ul>
